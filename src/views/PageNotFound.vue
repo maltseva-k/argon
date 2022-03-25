cc735f4c27ab9bd762d12dfda58831a6f94eb397
maltseva-k <k.obydennova@gmail.com>
2022-03-25 16:38:57 +0300

    update page-not-found

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="container bg-white flex justify-between ml-auto mr-auto mt-auto rounded-2xl p-10 w-1/2">
-    <div class="text-center font-sans font-bold text-8xl tracking-wide text-blue-500 ml-auto mr-auto">404</div>
-    <div class="text-medium text-xl font-sans align-middle mt-auto mb-auto">
-      <h1 class="mt-2">Страница не найдена</h1>
-      <p>К сожалению, страница, которую Вы запросили, не существует.</p>
-      <p>
-        Вы можете перейти на <router-link :to="{ name: 'home' }" class="hover:text-blue-500">главную страницу</router-link>.
-      </p>
+  <div class=" h-2/3 mt-10"></div>
+  <div class=" min-w-min bg-white ml-auto mr-auto mt-18 rounded-2xl p-10 w-1/3 shadow-md">
+    <div class="mt-2 text-center align-middle font-sans font-bold text-9xl tracking-wide text-blue-500 ml-auto mr-auto mb-20 ">404</div>
+    <div class="text-medium text-center text-xl font-sans align-middle mt-auto ml-auto mb-auto">
+      <h1 class="mt-2 font-bold text-lg mb-2">Page Not Found</h1>
+      <p>Looks like you've followed a broken link or entered a URL that doesn't exist on this site.</p>
+        <router-link :to="{ name: 'home' }" class="hover:underline text-blue-500 mt-5  w-full text-center -ml-5 pl-0 flex justify-center items-center font-bold">
+          <svg class="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
+          Back to our site
+        </router-link>.
     </div>
   </div>
 </template>
