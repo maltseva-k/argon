395814d919d4e5f2adb2d325cca219ebbec1d17a
maltseva-k <k.obydennova@gmail.com>
2022-03-31 15:03:04 +0300

    add vuelidate for register-form and update ux

--- src/components/layouts/SignUpLayout.vue
+++ src/components/layouts/SignUpLayout.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="h-screen relative w-full">
+  <div class="h-full relative w-full">
     <div class="h-96 bg-no-repeat bg-cover bg-center lg:rounded-3xl bg-top mt-5 mr-0 ml-0 lg:mr-5 lg:ml-5 bg-[url('/src/images/BackgroundCitySmall.png')]">
     </div>
     <main class="w-full absolute top-5">
         <main-loader v-if="isLoading" />
         <router-view />
     </main>
+    <div class="bottom-0 h-60 p-20 mb-10 bg-transparent">
+  </div>
   </div>
 </template>
 
--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="lg:w-1/3 sm:w-3/5 mx-auto w-4/5">
+    <div class="lg:w-1/3 sm:w-3/5 mx-auto w-4/5 h-full">
       <h1 class="font-bold text-white text-center text-2xl mb-1 lg:mb-1">Welcome!</h1>
       <p class="text-center text-white mb-1 lg:mb-2">
         Use these awesome forms to create new account in project for free.
