f9dd26932e68595417285c4bf9b78732f46fe151
maltseva-k <k.obydennova@gmail.com>
2022-04-01 14:59:54 +0300

    update logout button

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -39,7 +39,7 @@
           </router-link>
         </li>
       <li>
-        <div class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-gray-700">
+        <div @click="logout" class="cursor-pointer flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-gray-700">
           <div class="w-10 h-10 bg-blue-600 rounded-lg flex justify-center items-center align-middle text-white">
             <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path></svg>
           </div>
